80c25b6b76b0653a39d4f36328bca0e26c42704b
Brian Zinn <github@wakeskate.com>
2020-10-01 21:25:42 -0700

    add SceneContext (and Provider) - will allow use* hooks from react-babylonjs that are being split into new modules.

--- src/babylonjs-hook.tsx
+++ src/babylonjs-hook.tsx
@@ -1,5 +1,5 @@
+import React, { useEffect, useRef, useState, createContext } from 'react'
 import { Engine, Scene, Nullable, EngineOptions, SceneOptions } from '@babylonjs/core'
-import React, { useEffect, useRef } from 'react'
 
 export type BabylonjsProps = {
     antialias?: boolean
@@ -9,11 +9,31 @@ export type BabylonjsProps = {
     onSceneReady: (scene: Scene) => void
     onRender?: (scene: Scene) => void
     id: string
+    children: React.ReactNode
 };
+
+export type SceneContextType = {
+    engine: Nullable<Engine>
+    canvas: Nullable<HTMLCanvasElement | WebGLRenderingContext>
+    scene: Nullable<Scene>
+    sceneReady: boolean
+}
+
+const DEFAULT_CONTEXT: SceneContextType = {
+    engine: null,
+    canvas: null,
+    scene: null,
+    sceneReady: false
+}
+
+// TODO: build a fallback mechanism when typeof React.createContext !== 'function'
+export const SceneContext = createContext<SceneContextType>(DEFAULT_CONTEXT);
 
 export default (props: BabylonjsProps) => {
     const reactCanvas = useRef<Nullable<HTMLCanvasElement>>(null);
     const { antialias, engineOptions, adaptToDeviceRatio, sceneOptions, onRender, onSceneReady, ...rest } = props;
+
+    const [sceneContext, setSceneContext] = useState<SceneContextType>(DEFAULT_CONTEXT)
 
     useEffect(() => {
         if (reactCanvas.current) {
@@ -22,7 +42,15 @@ export default (props: BabylonjsProps) => {
             if (scene.isReady()) {
                 props.onSceneReady(scene)
             } else {
-                scene.onReadyObservable.addOnce((scene) => props.onSceneReady(scene));
+                scene.onReadyObservable.addOnce((scene) => {
+                    props.onSceneReady(scene);
+                    setSceneContext(() => ({
+                        canvas: reactCanvas.current,
+                        scene,
+                        engine,
+                        sceneReady: true,
+                    }));
+                });
             }
 
             engine.runRenderLoop(() => {
@@ -40,6 +68,13 @@ export default (props: BabylonjsProps) => {
                 window.addEventListener('resize', resize);
             }
 
+            setSceneContext(() => ({
+                canvas: reactCanvas.current,
+                scene,
+                engine,
+                sceneReady: scene.isReady(),
+            }));
+
             return () => {
                 scene.getEngine().dispose();
 
@@ -51,6 +86,11 @@ export default (props: BabylonjsProps) => {
     }, [reactCanvas])
 
     return (
-        <canvas ref={reactCanvas} {...rest} />
+        <>
+            <canvas ref={reactCanvas} {...rest} />
+            <SceneContext.Provider value={sceneContext}>
+                {props.children}
+            </SceneContext.Provider>
+        </>
     );
 }
