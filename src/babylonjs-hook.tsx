37c9c1856952c18056ddb1e61e781cc6bb06b463
Brian Zinn <github@wakeskate.com>
2021-09-28 16:14:57 -0700

    add ResizeObserver for canvas resize (window resize event listener misses canvas resize)

--- src/babylonjs-hook.tsx
+++ src/babylonjs-hook.tsx
@@ -12,14 +12,18 @@ export * from './engine';
 export * from './scene';
 
 export type BabylonjsProps = {
-    antialias?: boolean
-    engineOptions?: EngineOptions
-    adaptToDeviceRatio?: boolean
-    renderChildrenWhenReady?: boolean
-    sceneOptions?: SceneOptions
-    onSceneReady: (scene: Scene) => void
-    onRender?: (scene: Scene) => void
-    children?: React.ReactNode
+  antialias?: boolean
+  engineOptions?: EngineOptions
+  adaptToDeviceRatio?: boolean
+  renderChildrenWhenReady?: boolean
+  sceneOptions?: SceneOptions
+  onSceneReady: (scene: Scene) => void
+  /**
+   * Automatically trigger engine resize when the canvas resizes (default: true)
+   */
+  observeCanvasResize: boolean
+  onRender?: (scene: Scene) => void
+  children?: React.ReactNode
 };
 
 export type OnFrameRenderFn = (eventData: Scene, eventState: EventState) => void
@@ -33,22 +37,22 @@ export type OnFrameRenderFn = (eventData: Scene, eventState: EventState) => void
  * @param callOnce only call the callback once
  */
 export const useBeforeRender = (callback: OnFrameRenderFn, mask?: number, insertFirst?: boolean, callOnce?: boolean): void => {
-    const { scene } = useContext(SceneContext);
+  const { scene } = useContext(SceneContext);
 
-    useEffect(() => {
-        if (scene === null) {
-            return;
-        }
+  useEffect(() => {
+    if (scene === null) {
+      return;
+    }
 
-        const unregisterOnFirstCall: boolean = callOnce === true;
-        const sceneObserver: Nullable<Observer<Scene>> = scene.onBeforeRenderObservable.add(callback, mask, insertFirst, undefined, unregisterOnFirstCall);
+    const unregisterOnFirstCall: boolean = callOnce === true;
+    const sceneObserver: Nullable<Observer<Scene>> = scene.onBeforeRenderObservable.add(callback, mask, insertFirst, undefined, unregisterOnFirstCall);
 
-        if (unregisterOnFirstCall !== true) {
-            return () => {
-                scene.onBeforeRenderObservable.remove(sceneObserver);
-            }
-        }
-    })
+    if (unregisterOnFirstCall !== true) {
+      return () => {
+        scene.onBeforeRenderObservable.remove(sceneObserver);
+      }
+    }
+  })
 }
 
 /**
@@ -60,22 +64,22 @@ export const useBeforeRender = (callback: OnFrameRenderFn, mask?: number, insert
  * @param callOnce only call the callback once
  */
 export const useAfterRender = (callback: OnFrameRenderFn, mask?: number, insertFirst?: boolean, callOnce?: boolean): void => {
-    const { scene } = useContext(SceneContext);
+  const { scene } = useContext(SceneContext);
 
-    useEffect(() => {
-        if (scene === null) {
-            return;
-        }
+  useEffect(() => {
+    if (scene === null) {
+      return;
+    }
 
-        const unregisterOnFirstCall: boolean = callOnce === true;
-        const sceneObserver: Nullable<Observer<Scene>> = scene.onAfterRenderObservable.add(callback, mask, insertFirst, undefined, unregisterOnFirstCall);
+    const unregisterOnFirstCall: boolean = callOnce === true;
+    const sceneObserver: Nullable<Observer<Scene>> = scene.onAfterRenderObservable.add(callback, mask, insertFirst, undefined, unregisterOnFirstCall);
 
-        if (unregisterOnFirstCall !== true) {
-            return () => {
-                scene.onAfterRenderObservable.remove(sceneObserver);
-            }
-        }
-    })
+    if (unregisterOnFirstCall !== true) {
+      return () => {
+        scene.onAfterRenderObservable.remove(sceneObserver);
+      }
+    }
+  })
 }
 
 /**
@@ -88,127 +92,142 @@ export const useAfterRender = (callback: OnFrameRenderFn, mask?: number, insertF
  * @param panningMoustButton (ArcRotateCamera only)
  */
 export const useCamera = <T extends Camera>(createCameraFn: (scene: Scene) => T, autoAttach: boolean = true, noPreventDefault: boolean = true/*, useCtrlForPanning: boolean = false, panningMouseButton: number*/): Nullable<T> => {
-    const { scene } = useContext(SceneContext);
-    const cameraRef = useRef<Nullable<T>>(null);
+  const { scene } = useContext(SceneContext);
+  const cameraRef = useRef<Nullable<T>>(null);
 
-    useEffect(() => {
-        if (scene === null) {
-            console.warn('cannot create camera (scene not ready)');
-            return;
-        }
+  useEffect(() => {
+    if (scene === null) {
+      console.warn('cannot create camera (scene not ready)');
+      return;
+    }
 
-        const camera: T = createCameraFn(scene);
-        if (autoAttach === true) {
-            const canvas: HTMLCanvasElement = scene.getEngine().getRenderingCanvas()!;
+    const camera: T = createCameraFn(scene);
+    if (autoAttach === true) {
+      const canvas: HTMLCanvasElement = scene.getEngine().getRenderingCanvas()!;
 
-            // This attaches the camera to the canvas - adding extra parameters breaks backwards compatibility
-            // https://github.com/BabylonJS/Babylon.js/pull/9192 (keep canvas to work with < 4.2 beta-13)
-            // TODO: look at parameters of other camera types for attaching - likely need an 'options' parameter instead.
-            // if (camera instanceof ArcRotateCamera) {
-            //     camera.attachControl(noPreventDefault, useCtrlForPanning, panningMouseButton)
-            camera.attachControl(canvas, noPreventDefault);
-        }
-        cameraRef.current = camera;
+      // This attaches the camera to the canvas - adding extra parameters breaks backwards compatibility
+      // https://github.com/BabylonJS/Babylon.js/pull/9192 (keep canvas to work with < 4.2 beta-13)
+      // TODO: look at parameters of other camera types for attaching - likely need an 'options' parameter instead.
+      // if (camera instanceof ArcRotateCamera) {
+      //     camera.attachControl(noPreventDefault, useCtrlForPanning, panningMouseButton)
+      camera.attachControl(canvas, noPreventDefault);
+    }
+    cameraRef.current = camera;
 
-        return () => {
-            if (autoAttach === true) {
-                // canvas is only needed for < 4.1
-                const canvas: HTMLCanvasElement = scene.getEngine().getRenderingCanvas()!;
-                camera.detachControl(canvas);
-            }
-            camera.dispose();
-        }
-    }, [scene]);
+    return () => {
+      if (autoAttach === true) {
+        // canvas is only needed for < 4.1
+        const canvas: HTMLCanvasElement = scene.getEngine().getRenderingCanvas()!;
+        camera.detachControl(canvas);
+      }
+      camera.dispose();
+    }
+  }, [scene]);
 
-    return cameraRef.current;
+  return cameraRef.current;
 }
 
 export default (props: BabylonjsProps & React.CanvasHTMLAttributes<HTMLCanvasElement>) => {
-    const reactCanvas = useRef<Nullable<HTMLCanvasElement>>(null);
-    const { antialias, engineOptions, adaptToDeviceRatio, sceneOptions, onRender, onSceneReady, renderChildrenWhenReady, children, ...rest } = props;
+  const reactCanvas = useRef<Nullable<HTMLCanvasElement>>(null);
+  const { antialias, engineOptions, adaptToDeviceRatio, sceneOptions, onRender, onSceneReady, renderChildrenWhenReady, children, ...rest } = props;
 
-    const [sceneContext, setSceneContext] = useState<SceneContextType>({
-        scene: null,
-        sceneReady: false
-    });
+  const [sceneContext, setSceneContext] = useState<SceneContextType>({
+    scene: null,
+    sceneReady: false
+  });
 
-    const [engineContext, setEngineContext] = useState<EngineCanvasContextType>({
-        engine: null,
-        canvas: null
-    });
+  const [engineContext, setEngineContext] = useState<EngineCanvasContextType>({
+    engine: null,
+    canvas: null
+  });
 
-    useEffect(() => {
-        if (reactCanvas.current) {
-            const engine = new Engine(reactCanvas.current, antialias, engineOptions, adaptToDeviceRatio);
-            setEngineContext(() => ({
-                engine,
-                canvas: reactCanvas.current
-            }));
+  useEffect(() => {
+    if (reactCanvas.current) {
+      const engine = new Engine(reactCanvas.current, antialias, engineOptions, adaptToDeviceRatio);
+      setEngineContext(() => ({
+        engine,
+        canvas: reactCanvas.current
+      }));
 
-            const scene = new Scene(engine, sceneOptions);
-            const sceneIsReady = scene.isReady();
-            if (sceneIsReady) {
-                props.onSceneReady(scene);
-            } else {
-                scene.onReadyObservable.addOnce((scene) => {
-                    props.onSceneReady(scene);
-                    setSceneContext(() => ({
-                        canvas: reactCanvas.current,
-                        scene,
-                        engine,
-                        sceneReady: true,
-                    }));
-                });
-            }
+      let resizeObserver: Nullable<ResizeObserver> = null;
 
-            engine.runRenderLoop(() => {
-                if (scene.activeCamera) {
-                    if (typeof onRender === 'function') {
-                        onRender(scene);
-                    }
-                    scene.render();
-                } else {
-                    // @babylonjs/core throws an error if you attempt to render with no active camera.
-                    // if we attach as a child React component we have frames with no active camera.
-                    console.warn('no active camera..');
-                }
-            })
+      if (props.observeCanvasResize !== false && window.ResizeObserver) {
+        resizeObserver = new ResizeObserver(() => {
+          engine.resize();
+        });
+        resizeObserver.observe(reactCanvas.current);
+      }
 
-            const resize = () => {
-                scene.getEngine().resize();
-            }
 
-            if (window) {
-                window.addEventListener('resize', resize);
-            }
+      const scene = new Scene(engine, sceneOptions);
+      const sceneIsReady = scene.isReady();
+      if (sceneIsReady) {
+        props.onSceneReady(scene);
+      } else {
+        scene.onReadyObservable.addOnce((scene) => {
+          props.onSceneReady(scene);
+          setSceneContext(() => ({
+            canvas: reactCanvas.current,
+            scene,
+            engine,
+            sceneReady: true,
+          }));
+        });
+      }
 
-            setSceneContext(() => ({
-                canvas: reactCanvas.current,
-                scene,
-                engine,
-                sceneReady: sceneIsReady,
-            }));
-
-            return () => {
-                scene.getEngine().dispose();
-
-                if (window) {
-                    window.removeEventListener('resize', resize);
-                }
-            }
+      engine.runRenderLoop(() => {
+        if (scene.activeCamera) {
+          if (typeof onRender === 'function') {
+            onRender(scene);
+          }
+          scene.render();
+        } else {
+          // @babylonjs/core throws an error if you attempt to render with no active camera.
+          // if we attach as a child React component we have frames with no active camera.
+          console.warn('no active camera..');
         }
-    }, [reactCanvas]);
+      })
 
-    return (
-        <>
-            <canvas ref={reactCanvas} {...rest} />
-            <EngineCanvasContext.Provider value={engineContext}>
-                <SceneContext.Provider value={sceneContext}>
-                    {(renderChildrenWhenReady !== true || (renderChildrenWhenReady === true && sceneContext.sceneReady)) &&
-                        children
-                    }
-                </SceneContext.Provider>
-            </EngineCanvasContext.Provider>
-        </>
-    );
+      const resize = () => {
+        scene.getEngine().resize();
+      }
+
+      if (window) {
+        window.addEventListener('resize', resize);
+      }
+
+      setSceneContext(() => ({
+        canvas: reactCanvas.current,
+        scene,
+        engine,
+        sceneReady: sceneIsReady,
+      }));
+
+      return () => {
+        // cleanup
+        if (resizeObserver !== null) {
+          resizeObserver.disconnect();
+        }
+
+        if (window) {
+          window.removeEventListener('resize', resize);
+        }
+
+        scene.getEngine().dispose();
+      }
+    }
+  }, [reactCanvas]);
+
+  return (
+    <>
+      <canvas ref={reactCanvas} {...rest} />
+      <EngineCanvasContext.Provider value={engineContext}>
+        <SceneContext.Provider value={sceneContext}>
+          {(renderChildrenWhenReady !== true || (renderChildrenWhenReady === true && sceneContext.sceneReady)) &&
+            children
+          }
+        </SceneContext.Provider>
+      </EngineCanvasContext.Provider>
+    </>
+  );
 }
